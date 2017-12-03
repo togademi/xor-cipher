9951af3c41929d1a332e41f285fcec8bb810a4e2
tomas <togademi@gmail.com>
2017-12-03 21:44:31 +0100

    Start

---
+++ src/xorcipher.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef unsigned char byte;
+
+int main() {
+  char coder[] = "hola";
+  char decoder[] = "adios";
+  char clef[] = "key";
+
+  return 0;
+}
